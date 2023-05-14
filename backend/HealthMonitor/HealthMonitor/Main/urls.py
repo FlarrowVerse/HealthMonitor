3c27c8b0327ae972b97587b6545508777a1e79e5
Archisman Chakraborty <archisman1999@gmail.com>
2023-05-14 22:23:10 +0530

    Probable URL paths setup in Main app.

--- backend/HealthMonitor/HealthMonitor/Main/urls.py
+++ backend/HealthMonitor/HealthMonitor/Main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from Main import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('dashboard', views.dashboard, name='user-dash'),
+	path('patient/<int:patient_id>', views.patient_details_view, name='patient-dash'),
+	path('patient/<int:patient_id>/records', views.patient_record_view, name='record'),
 ]
--- backend/HealthMonitor/HealthMonitor/Main/views.py
+++ backend/HealthMonitor/HealthMonitor/Main/views.py
@@ -8,3 +8,21 @@ def index(request):
 	<h1>This is the Main application</h1>
 	<a href="http://localhost:8000/">Go to SignUp App</a>
 	''')
+
+def dashboard(request):
+	content = {'page_desc': "This is going to be a user dashboard page"}
+	return render(request, 'main/dashboard.html', context=content)
+
+def patient_details_view(request, patient_id):
+	content = {
+		'page_desc': f"This is going to be a patient dashboard page for patient with id {patient_id}"
+	}
+	return render(request, 'main/patientDetails.html', context=content)
+
+def patient_record_view(request, patient_id):
+	record_date = request.GET['recid']
+	content = {
+		'patient_id': patient_id,
+		'record_date': record_date
+	}
+	return render(request, 'main/patientRecord.html', context=content)
